1004c378d4b11ce7cdb4d333c38f4ed44a6a9da7
ennth <39110859+aemaeth0@users.noreply.github.com>
2018-10-07 17:42:22 -0500

    updating android login system

--- FinalAndroidProject/app/src/main/java/com/example/xxaemaethxx/cs354final/MainActivity.java
+++ FinalAndroidProject/app/src/main/java/com/example/xxaemaethxx/cs354final/MainActivity.java
@@ -21,11 +21,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // instance variables
         Name = (EditText) findViewById(R.id.etName);
         Password = (EditText) findViewById(R.id.etPassword);
         Login = (Button) findViewById(R.id.btnLogin);
         Info = (TextView) findViewById(R.id.tvInfo);
+        Info.setText("Number of attempts remaining: 5");
 
+        // creating onClickListener when login button is clicked to run validate method
         Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -35,17 +38,18 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-
+        // validate method
     private void validate(String userName, String userPassword) {
         if (userName.equals("Admin") && userPassword.equals("1234"))
         {
             Intent intent = new Intent(MainActivity.this, WelcomeScreen.class);
+            intent.putExtra("NAME", Name.getText().toString());
             startActivity(intent);
         }
     else
         {
             counter--;
-            Info.setText("Number of attempts left is: " + counter);
+            Info.setText("Number of attempts remaining: " + counter);
         }
         if (counter == 0){
             Login.setEnabled(false);
--- FinalAndroidProject/app/src/main/java/com/example/xxaemaethxx/cs354final/WelcomeScreen.java
+++ FinalAndroidProject/app/src/main/java/com/example/xxaemaethxx/cs354final/WelcomeScreen.java
@@ -1,13 +1,26 @@
 package com.example.xxaemaethxx.cs354final;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class WelcomeScreen extends AppCompatActivity {
+
+    private TextView tvWelcome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_screen);
+
+        tvWelcome = (TextView) findViewById(R.id.tvWelcome);
+
+        Intent intent  = getIntent();
+        String name = intent.getStringExtra("NAME");
+
+        tvWelcome.setText("Hey " + name + "!, you successfully logged in!");
+
+
     }
 }
